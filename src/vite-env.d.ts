308d758831512f353996fff7cdce2fb8f9fd7b81
Avimitin <avimitin@gmail.com>
2022-10-07 22:48:32 +0800

    Add custom env variable
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly RGIT_APP_TITLE: string
+    readonly RGIT_BACKEND_ADDR: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
