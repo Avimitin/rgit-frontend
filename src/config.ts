413f07118a91369d2bf3aefa4caa04d80f077f22
Avimitin <avimitin@gmail.com>
2022-10-07 22:48:56 +0800

    Use the new prefix as env
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-import * as dotenv from "dotenv";
-
 export interface Configuration {
     backend: {
         base_url: string,
@@ -8,7 +6,7 @@ export interface Configuration {
 }
 
 export default function createConfig(): Configuration {
-    const backend_url = import.meta.env.VITE_RGIT_BACKEND_URL;
+    const backend_url = import.meta.env.RGIT_BACKEND_ADDR;
 
     return {
         backend: {
